ad307f5679a630c0bd6786ea94621f736d83be80
Mike Lynch <mike.lynch@iinet.net.au>
2017-06-03 18:51:13 +1000

    Added missing script

---
+++ Scripts/background.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+geom=$1
+mode=$2
+att=$3
+working="./working"
+blend=$4
+gradient=$5
+level=$6
+blur=$7
+file=$8
+
+echo "geom ${geom}"
+echo "perlin mode ${mode}"
+echo "attenuation ${att}"
+echo "blend ${blend}"
+echo "gradient ${gradient}"
+echo "levels ${level}"
+echo "blur ${blur}"
+
+/Users/mike/Desktop/DeepDream/deepdream/scripts/perlin.sh ${geom} -m ${mode} -a ${att} ${working}/perlin.jpg
+
+convert -size ${geom} ${gradient} ${working}/fade.jpg 
+composite -blend ${blend} ${working}/perlin.jpg ${working}/fade.jpg ${working}/comp.jpg
+convert -level ${level} -blur ${blur} ${working}/comp.jpg ${file}
+convert ${file} -colorspace rgb -type truecolor ${file}
+
+echo ${file}
+
