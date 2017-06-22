fcfa39171b63feca53172a40dcd41c78731d49f3
Mike Lynch <mike.lynch@iinet.net.au>
2017-06-22 15:31:43 +1000

    When zooming, now readjusts the image size - for use with deepdraw zooms that blow up the input image

--- dive.py
+++ dive.py
@@ -12,8 +12,8 @@ model = 'manga_tag'
 recipe = range(512)
 iters = '2'
 octaves = '5'
-dd_octaves = '../neuralgae/src/Control/Renderers/manga_big_grad.json'
-frames = '20'
+dd_octaves = '../neuralgae/src/Control/Renderers/manga_quick_scale.json'
+frames = '48'
 basefile = 'Dive6/manga'
 startframe = 0
 zoom = '0.05'
@@ -29,12 +29,15 @@ for target in recipe:
     a = [ script, "--model", model, "--target", str(target), "--basefile", basefile, "--deepdraw", dd_octaves, "--frames", frames, "--zoom", zoom, "--initial", str(i), origfile, outdir ]
     print ' '.join(a)
     subprocess.call(a)
+    sys.exit()
     if os.path.isfile(newfile):
         rfile = newfile + '.resize.jpg'
+        print "found newfile, resizing to {}".format(rfile)
         r = [ 'convert', '-resize', origsize, newfile, rfile  ]
         subprocess.call(r)
         origfile = rfile
         i += f
     else:
+        print "using lastfile {}".format(lastfile)
         origfile = lastfile
 
