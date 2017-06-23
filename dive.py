6b70c35513f10d1f7060321b981fc65409771379
Mike Lynch <mike.lynch@iinet.net.au>
2017-06-23 22:19:52 +1000

    Modified dive.py to work with googlenet

--- dive.py
+++ dive.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python2
 
+
 import sys, os, os.path
 import subprocess
 import string
 
-origfile = 'Input/desaturated.jpg'
-origsize = '224x224'
+#origfile = 'Input/desaturated.jpg'
+#origsize = '224x224'
+origfile = 'Input/base.jpg'
+origsize = '320x240'
 outdir = 'Output'
 script = './dream.py'
-model = 'manga_tag'
+# model = 'manga_tag'
+model = 'googlenet'
 recipe = range(512)
 iters = '2'
 octaves = '5'
-dd_octaves = '../neuralgae/src/Control/Renderers/manga_quick_scale.json'
-frames = '2'
-basefile = 'Dive7/manga'
+# dd_octaves = '../neuralgae/src/Control/Renderers/manga_quick_scale.json'
+dd_octaves = '../neuralgae/src/Control/Renderers/googlenet_quick.json' 
+frames = '12'
+basefile = 'Dive8/google'
 startframe = 0
-zoom = '0.05'
+zoom = '0.1'
+rotate = '2' 
 
 
 
@@ -26,7 +32,7 @@ lastfile = origfile
 
 for target in recipe:
     newfile = os.path.join(outdir, basefile + ('_f%d.jpg' % (i + f - 1)))
-    a = [ script, "--model", model, "--target", str(target), "--basefile", basefile, "--deepdraw", dd_octaves, "--frames", frames, "--zoom", zoom, "--initial", str(i), origfile, outdir ]
+    a = [ script, "--model", model, "--target", str(target), "--basefile", basefile, "--deepdraw", dd_octaves, "--frames", frames, "--zoom", zoom, "--rotate", rotate, "--initial", str(i), origfile, outdir ]
     print ' '.join(a)
     subprocess.call(a)
     if os.path.isfile(newfile):
